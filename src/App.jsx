b43caf18bad6710ca77aee14df91fcc94aaed01a
Muhammad Awais <awais619261@gmail.com>
2024-11-19 21:29:40 +0500

    bmi

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import './mystyle.css'
+import MyButton from './MyButton';
+import { useState } from 'react';
+
+function App() {
+  let message;
+  const [weight,setWeight]= useState()
+  const [height,setheight]= useState()
+  const [bmi,setBmi]= useState()
+  const bmicalculation=()=>{
+    let bmi=0;
+    bmi = weight/(height)**2;
+    alert('your bmi is'+bmi);
+    setBmi(bmi)
+
+
+  } 
+  if(bmi>0 && bmi<18.5 ){
+    message= 'you are underwight'
+  }
+  else if(bmi>= 18.5 && bmi<=24.9){
+    message='you are healthy'
+  }
+  else if(bmi>24.9){
+    message='you are overweight'
+  }
+  
+
+  return (
+  
+    <div className="body">
+    <h1>BMI Calculator</h1>
+    <form>
+      
+        <label>Enter Weight in KG</label>
+        <input
+          placeholder="Enter your Weight"
+          type="number"
+          value={weight}
+          onChange={(txt) => setWeight(txt.target.value)}
+          className="inputs"
+        />
+     
+      
+        <label>Enter your Height in meters</label>
+        <input
+          placeholder="Enter your Height"
+          value={height}
+          onChange={(txt) => setheight(txt.target.value)}
+          className="inputs"
+        />
+      
+    </form>
+    <div style={{ display: 'flex', gap: '10px' }}>
+      <MyButton title={'Reload'} onClick={bmicalculation} />
+      <MyButton title={'Calculate'} onClick={bmicalculation} />
+    </div>
+    <div>
+      <h3>{bmi && `Your BMI: ${bmi.toFixed(2)}`}</h3>
+      <h2>{message}</h2>
+    </div>
+  </div>
+  
+  );
+}
+
+export default App;
---
+++ src/MyButton.jsx
@@ -0,0 +1,4 @@
+function MyButton({title,onClick}) {
+  return <button style={{ borderRadius:12,margin:5}} onClick={onClick}>{title}</button>;
+}
+export default MyButton;
